6e37e596bfdb41270fe84087cced9006caf1e109
karankharole <karankharole2000@gmail.com>
2022-12-09 22:36:10 +0530

    create simple linked list and insert elements

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            linkedList l_Object = new linkedList();
+            l_Object.add(56);
+            l_Object.add(30);
+            l_Object.add(70);
+            l_Object.display();
+        }
+    }
+}
